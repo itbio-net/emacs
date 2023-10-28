9aabf5f7275d11d5d04109203695edf619b9d41a
doyoung song <dy@doyoungui-MacBookPro.local>
2023-10-29 00:55:12 +0900

    MnemonicSlugs를 이용

---
+++ lib/chat_web/live/room_live.ex
@@ -0,0 +1,30 @@
+defmodule ChatWeb.RoomLive do
+  use ChatWeb, :live_view
+  # require Logger
+
+  @impl true
+  def mount(%{"id" => room_id}, _session, socket) do
+    topic = "room:" <> room_id
+    if connected?(socket), do:  ChatWeb.Endpoint.subscribe(topic)
+
+    {:ok,
+     assign(socket,
+       room_id: room_id,
+       topic: topic,
+       messages: [%{uuid: UUID.uuid4(), content: "Twitch joined the chat"}],
+       temporary_assigns: [messages: []]
+     )}
+  end
+
+  @impl true
+  def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
+    message = %{uuid: UUID.uuid4(), content: message}
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    {:noreply, assign(socket, messages: [message])}
+  end
+end
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Chat.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+#      compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -46,7 +46,8 @@ defmodule Chat.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:mnemonic_slugs, "~> 0.0.3"}
+      {:mnemonic_slugs, "~> 0.0.3"},
+      {:uuid, "~> 1.1.8"}
     ]
   end
 
